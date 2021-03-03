46653ab12a4d80049c350440e31e0f66bed389b1
chao.wang1 <chao.wang1@thoughtworks.com>
2021-03-03 11:31:33 +0800

    updated

--- example/lib/video_page.dart
+++ example/lib/video_page.dart
@@ -2,30 +2,37 @@ import 'package:ff_video/controls/tencent_video/tencent_video_control.dart';
 import 'package:ff_video/ff_video.dart';
 import 'package:flutter/material.dart';
 
-class VideoPage extends StatelessWidget {
+class VideoPage extends StatefulWidget {
   final String url;
   final String title;
 
-  const VideoPage({Key key, this.url, this.title}) : super(key: key);
+   VideoPage({Key key, this.url, this.title}) : super(key: key);
+
+  @override
+  _VideoPageState createState() => _VideoPageState();
+}
+
+class _VideoPageState extends State<VideoPage> {
+  VideoPlayerController controller;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: ListView(
         children: [
           FFVideo(
-            url: url,
+            url: widget.url,
             autoPlay: true,
+            onReceiveController: (controller) {
+              this.controller = controller;
+            },
             control: TencentVideoControl(
-              thumbnail: Image.network(
-                'http://img.mp.itc.cn/upload/20170326/39eaf2c41cfb46fe98982670af385a64_th.jpeg',
-                fit: BoxFit.cover,
-              ),
+              thumbnailUrl: 'http://img.mp.itc.cn/upload/20170326/39eaf2c41cfb46fe98982670af385a64_th.jpeg',
               title: Text(
-                title,
+                widget.title,
                 style: TextStyle(
                   color: Colors.white,
                 ),
